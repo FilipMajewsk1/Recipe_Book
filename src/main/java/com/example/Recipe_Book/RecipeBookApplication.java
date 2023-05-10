fa32fb2a9ced783fb6ca483ea13b27de6cc91539
FilipMajewsk1 <filip.majewski.022@gmail.com>
2023-05-10 10:19:00 +0200

    added swagger-ui

--- src/main/java/com/example/Recipe_Book/RecipeBookApplication.java
+++ src/main/java/com/example/Recipe_Book/RecipeBookApplication.java
@@ -3,11 +3,10 @@ package com.example.Recipe_Book;
 import com.example.Recipe_Book.recipe.Recipe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+
 import java.util.List;
 @SpringBootApplication
-
 public class RecipeBookApplication {
 
 	public static void main(String[] args) {
